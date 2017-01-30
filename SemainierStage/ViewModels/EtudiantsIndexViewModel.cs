903a357c16c9e020acb827a0db0915b9f9cf774d
AlexandreCloutier <cloutier.alexandre@carrefour.cegepvicto.ca>
2017-01-30 13:56:36 -0500

    Revert "Revert "Ajout du projet""
    This reverts commit a6e7e587760bcab0bb523ef54141158922838ffd.

---
+++ SemainierStage/ViewModels/EtudiantsIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SemainierStage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SemainierStage.ViewModels
+{
+    public class EtudiantsIndexViewModel
+    {
+        public IEnumerable<Session> session { get; set; }
+        public IEnumerable<Etudiant> etudiant { get; set; }
+    }
+}
